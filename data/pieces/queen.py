a42dd845defa0f220d912679471ab497c17e7db9
Yurii <yurchik1997yurchik@gmail.com>
2023-11-20 19:19:13 +0300

    description of chess pieces

---
+++ data/pieces/queen.py
@@ -0,0 +1,39 @@
+"""Описание фигуры 'Королева'"""
+from data.chess_piece import ChessPiece
+from data.move import Move
+from data.position import Position
+from data.vector2 import Vector2
+
+
+class Queen(ChessPiece):
+    __name = 'Q'
+
+    __moves: list[Move] = [
+        Move(change=Vector2(vertical=1, horizontal=1), is_direction=True),
+        Move(change=Vector2(vertical=1, horizontal=0), is_direction=True),
+        Move(change=Vector2(vertical=1, horizontal=-1), is_direction=True),
+        Move(change=Vector2(vertical=0, horizontal=1), is_direction=True),
+        Move(change=Vector2(vertical=0, horizontal=-1), is_direction=True),
+        Move(change=Vector2(vertical=-1, horizontal=-1), is_direction=True),
+        Move(change=Vector2(vertical=-1, horizontal=0), is_direction=True),
+        Move(change=Vector2(vertical=-1, horizontal=1), is_direction=True),
+    ]
+
+    def __init__(
+        self,
+        position: Position,
+        chess_id: str | None = None,
+    ):
+        if chess_id is None:
+            super().__init__(
+                name=Queen.__name,
+                moves=Queen.__moves,
+                position=position,
+            )
+        else:
+            super().__init__(
+                name=Queen.__name,
+                moves=Queen.__moves,
+                position=position,
+                chess_id=chess_id,
+            )
