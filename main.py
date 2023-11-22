c2dd828df3e2e80d4e53f87dfbe7845e74453c73
Yurii <yurchik1997yurchik@gmail.com>
2023-11-22 19:51:41 +0300

    modified

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from utils.board_visualizer import board_visualizer
-from utils.piece_storage import PieceStorage
-from notation.fen_notation_parser import FenNotationParser
+from console.console_interface import ConsoleInterface
 
-
-if __name__ == '__main__':
-    fen = "Q6Q/8/8/3QQ3/3QQ3/8/8/Q6Q"
-    parser = FenNotationParser()
-    storage = PieceStorage(is_standart=True)
-    board = parser.parse(fen, storage)
-    print(board_visualizer(board))
-    print(parser.serialize(board))
+if __name__ == "__main__":
+    try:
+        console = ConsoleInterface()
+        console.run()
+    except KeyboardInterrupt:
+        pass
+    except RuntimeError:
+        pass
